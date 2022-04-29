599e503db83cbdba8f3486ad3beab26b729adeea
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-04-29 16:49:44 -0300

    add supabase auth state change cb func

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <script setup lang="ts">
 	import { RouterView } from 'vue-router';
 	import Layout from './components/Layout/Layout.vue';
+	import { client } from './lib/supabase';
+	import { useAuthStore } from './store';
+
+	client.auth.onAuthStateChange((event, session) => {
+		if (event === 'SIGNED_OUT') {
+			useAuthStore().user = null;
+		}
+
+		if (event === 'SIGNED_IN' && session) {
+			useAuthStore().user = session?.user;
+		}
+	});
 </script>
 
 <template>
