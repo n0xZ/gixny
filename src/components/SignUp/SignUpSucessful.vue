a2c89e6173fb0a58393de3d5a32056217343848b
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-04-29 16:50:07 -0300

    add sucessful sign up component

---
+++ src/components/SignUp/SignUpSucessful.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+	interface Props {
+		isLoginSucessful: boolean;
+	}
+	const props = defineProps<Props>();
+</script>
+<template>
+	<article v-if="props.isLoginSucessful">
+		<p>
+			Por favor, revise su mail para confirmar su registro.
+		</p>
+	</article>
+</template>
