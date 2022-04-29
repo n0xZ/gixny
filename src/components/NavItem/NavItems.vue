a590da25348e1f9ba466ae229b0f8dfbcedd1fbe
n0xZ <o.gonzalo@hotmail.com>
2022-04-28 22:54:05 -0300

    add nav items based on auth

---
+++ src/components/NavItem/NavItems.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+	import NavLink from '../NavLink/index.vue';
+	import { useAuthStore } from '@/store/index';
+	const isAuth = useAuthStore().isAuthenticated;
+</script>
+
+<template>
+	<ul class="flex flex-row items-center" v-if="!isAuth">
+		<li class="mx-2">
+			<NavLink href="/signup">Crear usuario</NavLink>
+		</li>
+		<li class="mx-2">
+			<NavLink href="/signin">Iniciar sesión</NavLink>
+		</li>
+	</ul>
+	<ul class="flex flex-row items-center" v-if="isAuth">
+		<li class="mx-2">
+			<NavLink href="/signup"> Mi perfíl</NavLink>
+		</li>
+		<li>
+			<button
+				class="px-5 py-3 rounded-lg text-white bg-zinc-900"
+			>
+				Cerrar sesión
+			</button>
+		</li>
+	</ul>
+</template>
