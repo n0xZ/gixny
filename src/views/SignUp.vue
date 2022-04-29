08bf7f36a1a3bef302604b4b8d3047f9586552b6
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-04-29 16:27:54 -0300

    set signIn component witn its login

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,8 +1,53 @@
-<script setup lang="ts"></script>
-
+<script setup lang="ts">
+	import { ref } from 'vue';
+	import { useForm } from 'vee-validate';
+	import { toFormValidator } from '@vee-validate/zod';
+	import FormField from '../components/Form/FormField.vue';
+	import SignUpSucessful from '@/components/SignUp/SignUpSucessful.vue';
+	import { useAuthStore } from '@/store/index';
+	import { loginSchema } from '@/utils/zod';
+	import type { Login } from '../types';
+	const store = useAuthStore();
+	const isSignUpSuccessFul = ref(false);
+	const { errors, handleSubmit } = useForm<Login>({
+		validationSchema: toFormValidator(loginSchema),
+	});
+	const onSubmit = handleSubmit(async (values) => {
+		await store.signUp(values);
+		if (store.user) {
+			isSignUpSuccessFul.value = true;
+		}
+	});
+</script>
 
 <template>
+	<section class="container mx-auto max-w-4xl">
+		<article v-if="!isSignUpSuccessFul">
+			<h1 class="text-center text-2xl mb-3">Crear cuenta</h1>
+			<form @submit="onSubmit">
+				<FormField
+					:type="'text'"
+					:name="'email'"
+					:label="'Correo electrónico'"
+					:placeholder="'Por ej... elmaildepepito123@gmail.com'"
+					:error="errors.email"
+				/>
+				<FormField
+					:name="'password'"
+					:type="'password'"
+					:label="'Contraseña'"
+					:placeholder="'Por ej... lacontraseñadepepito123*'"
+					:error="errors.password"
+				/>
+				<button
+					type="submit"
+					class="px-5 py-3 rounded-md bg-zinc-900 text-white"
+				>
+					Crear nueva cuenta
+				</button>
+			</form>
+		</article>
 
-
-
+		<SignUpSucessful :isLoginSucessful="isSignUpSuccessFul" />
+	</section>
 </template>
