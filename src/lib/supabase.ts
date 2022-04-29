79215b45053217a510fbeb03f2dcce85d14eb8dd
n0xZ <o.gonzalo@hotmail.com>
2022-04-28 23:00:13 -0300

    set new env imports

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const API_URL = String(import.meta.env.SUPABASE_URL);
-const API_KEY = String(import.meta.env.SUPABASE_API_KEY);
+const API_URL = String(import.meta.env.VITE_SUPABASE_URL);
+const API_KEY = String(import.meta.env.VITE_SUPABASE_ANON_KEY);
 
 export const client = createClient(API_URL, API_KEY);
