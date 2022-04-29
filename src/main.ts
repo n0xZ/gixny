1c36f0ec0fbc9a25515fcb9604b077199bdffd1e
n0xZ <o.gonzalo@hotmail.com>
2022-04-28 22:57:59 -0300

    add toast and pinia on app root

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue';
+import { createPinia, PiniaVuePlugin } from 'pinia';
+import Toast from 'vue-toastification';
 import App from './App.vue';
 import { router } from './routes';
 
-createApp(App).use(router).mount('#app');
+createApp(App)
+	.use(PiniaVuePlugin)
+	.use(createPinia())
+	.use(router)
+	.use(Toast)
+	.mount('#app');
