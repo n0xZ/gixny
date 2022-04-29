91b3aed379b898adc617010166264e6f652591fd
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-04-29 16:51:16 -0300

    add features related to store

--- src/components/NavItem/NavItems.vue
+++ src/components/NavItem/NavItems.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 	import NavLink from '../NavLink/index.vue';
 	import { useAuthStore } from '@/store/index';
-	const isAuth = useAuthStore().isAuthenticated;
+
+	const store = useAuthStore();
 </script>
 
 <template>
-	<ul class="flex flex-row items-center" v-if="!isAuth">
+	<ul class="flex flex-row items-center" v-if="!store.user">
 		<li class="mx-2">
 			<NavLink href="/signup">Crear usuario</NavLink>
 		</li>
@@ -13,13 +14,14 @@
 			<NavLink href="/signin">Iniciar sesión</NavLink>
 		</li>
 	</ul>
-	<ul class="flex flex-row items-center" v-if="isAuth">
+	<ul class="flex flex-row items-center" v-if="store.user">
 		<li class="mx-2">
 			<NavLink href="/signup"> Mi perfíl</NavLink>
 		</li>
 		<li>
 			<button
 				class="px-5 py-3 rounded-lg text-white bg-zinc-900"
+				@click="store.signOut"
 			>
 				Cerrar sesión
 			</button>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+	import { useAuthStore } from '../store';
+	const loggedUser = useAuthStore().user;
+</script>
+
+<template>
+	<section>
+		<h1>Bienvenido, {{ loggedUser?.email }}</h1>
+	</section>
+</template>
