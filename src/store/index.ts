6f883e3b64058c708e2663d366dce441c8863a30
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-04-29 16:49:24 -0300

    add sign up action on store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,32 +1,45 @@
 import { defineStore } from 'pinia';
+import { User, ApiError } from '@supabase/supabase-js';
 import { client } from '@/lib/supabase';
-import { User } from '@supabase/supabase-js';
 import type { Credentials } from 'src/types';
+
 export const useAuthStore = defineStore('Auth', {
 	state: () => {
 		return {
 			user: null as User | null,
+			error: null as ApiError | null,
 		};
 	},
 	getters: {
 		isAuthenticated: (state) => (state.user ? true : false),
 	},
 	actions: {
-		async login(values: Credentials) {
-			const { error, user } = await client.auth.signIn({
+		async signIn(values: Credentials) {
+			const { error, user } =
+				await client.auth.signIn({
+					email: values.email,
+					password: values.password,
+				});
+			if (user && !error) {
+				this.user = user;
+			}
+			this.error = error;
+		},
+		signOut() {
+			if (this.user) {
+				client.auth.signOut();
+				this.user = null;
+			}
+		},
+		async signUp(values: Credentials) {
+			const { error, user } = await client.auth.signUp({
 				email: values.email,
 				password: values.password,
 			});
 			if (user && !error) {
 				this.user = user;
 			}
-			throw new Error(error?.message);
-		},
-		logout() {
-			if (this.user) {
-				client.auth.signOut();
-				this.user = null;
-			}
+			this.error = error;
 		},
 	},
 });
