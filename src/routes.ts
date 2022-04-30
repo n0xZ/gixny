4aa821d3d0dc8e157d7e1fa5d978e162465a4ec1
n0xZ <o.gonzalo@hotmail.com>
2022-04-30 17:18:42 -0300

    add privates routes based on useAuth store

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { useAuthStore } from './store';
 import Home from './views/Home.vue';
 
 export const router = createRouter({
@@ -6,16 +7,52 @@ export const router = createRouter({
 	routes: [
 		{
 			path: '/',
+			name: 'PublicHome',
 			component: Home,
+			meta: {
+				requiresAuth: false,
+			},
 		},
 		{
 			path: '/signin',
+			name: 'SignIn',
 			component: () => import('./views/SignIn.vue'),
+			meta: {
+				requiresAuth: false,
+			},
 		},
 
 		{
 			path: '/signup',
+			name: 'SignUp',
 			component: () => import('./views/SignUp.vue'),
+			meta: {
+				requiresAuth: false,
+			},
+		},
+		{
+			path: '/dashboard',
+			name: 'Dashboard',
+
+			component: () => import('./views/Dashboard.vue'),
+			meta: {
+				requiresAuth: true,
+			},
+		},
+		{
+			path: '/profile',
+			name: 'Profile',
+			component: () => import('./views/Profile.vue'),
+			meta: {
+				requiresAuth: true,
+			},
 		},
 	],
+});
+
+router.beforeEach((to, from, next) => {
+	const store = useAuthStore();
+	if (to.meta.requiresAuth && !store.isAuthenticated) {
+		next({ name: 'SignIn' });
+	} else next();
 });
