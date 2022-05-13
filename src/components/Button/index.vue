15e899a55171752b1a1c77455c8480a36f2c4bdf
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-05-13 16:28:58 -0300

    set reusable button component

---
+++ src/components/Button/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+	import { ButtonHTMLAttributes } from 'vue';
+	interface Props extends ButtonHTMLAttributes {}
+
+	const props = defineProps<Props>();
+</script>
+
+<template>
+	<button v-bind="props" class="px-5 py-3 rounded-md bg-zinc-900 text-white">
+		<slot></slot>
+	</button>
+</template>
