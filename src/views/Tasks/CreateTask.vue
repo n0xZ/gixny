dba04802bf79eb32e2d04d5c9ffea64e0253f71b
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-05-13 16:21:15 -0300

    set create/update tasks views

--- src/views/Tasks/CreateTask.vue
+++ src/views/Tasks/CreateTask.vue
@@ -2,21 +2,43 @@
 	import { useForm } from 'vee-validate';
 	import { toFormValidator } from '@vee-validate/zod';
 	import FormField from '@/components/Form/FormField.vue';
+	import FormTextArea from '@/components/Form/FormTextArea.vue';
 	import Hero from '@/components/Hero/index.vue';
-    import {} from '@/utils/zod'
-import type {TaskFormFields} from '../../types'
-
+	import { taskSchema } from '@/utils/zod';
+	import type { TaskFormFields } from '../../types';
+	import Button from '@/components/Button/index.vue';
+	const { errors, handleSubmit, isSubmitting } = useForm<TaskFormFields>({
+		validationSchema: toFormValidator(taskSchema),
+	});
+	const onSubmit = handleSubmit((values) => {
+		console.log(values);
+	});
 </script>
 
 <template>
 	<Hero>
-		<form>
+		<form @submit="onSubmit">
 			<FormField
-               
+				:error="errors.title"
+				:type="'text'"
 				:label="'Titulo de tarea'"
 				:placeholder="'Por ej... Mi primer tarea'"
 				:name="'title'"
 			/>
+			<FormTextArea
+				:error="errors.description"
+				:label="'Titulo de tarea'"
+				:placeholder="'Por ej... la descripción de mi tarea'"
+				:name="'description'"
+			/>
+			<FormField
+				:error="errors.priority"
+				:type="'text'"
+				:label="'Prioridad de la tarea'"
+				:placeholder="'Por ej... Mi primer tarea'"
+				:name="'priority'"
+			/>
+			<button type="submit" :disabled="isSubmitting">Crear tarea</button>
 		</form>
 	</Hero>
 </template>
