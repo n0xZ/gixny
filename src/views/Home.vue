7259068ba3373b135e49b9adcd1239a0b39bcb46
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-05-31 16:52:04 -0300

    set initial home styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,5 +5,22 @@
 </script>
 
 <template>
-	<section>Bienvenido a Gixny!</section>
+	<section :class="$style.home__hero">
+		<h1>Bienvenido a Gixny!</h1>
+		<p>Plataforma para la creación de tareas personales</p>
+	</section>
 </template>
+
+<style module>
+	.home__hero {
+		display: grid;
+		min-height: 100vh;
+		grid-template-columns: repeat(2, 1fr);
+		grid-template-rows: auto;
+		place-items: center;
+	}
+
+	
+	.home__paragraph {
+	}
+</style>
