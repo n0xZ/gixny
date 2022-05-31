3abb1b2ef6403d4f6dd2b181734b05549ba88f44
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-05-31 16:52:45 -0300

    add custom validatino message on loginSchema

--- src/utils/zod.ts
+++ src/utils/zod.ts
@@ -1,8 +1,11 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-	email: z.string().email().min(10,{message:'Este campo es requerido'}),
-	password: z.string().min(3),
+	email: z
+		.string()
+		.min(10, { message: 'Campo requerido' })
+		.email({ message: 'El formato de mail ingresado no es válido' }),
+	password: z.string().min(3, { message: 'Campo requerido' }),
 });
 
 export const taskSchema = z.object({
