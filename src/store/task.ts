4a8c523acc9d9ccb3d7c7304e48f02962632b7b7
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-05-31 11:49:08 -0300

    set task initial store

---
+++ src/store/task.ts
@@ -0,0 +1,29 @@
+import { defineStore } from 'pinia';
+import { client } from '@/lib/supabase';
+import { Task } from '../types';
+
+export const taskStore = defineStore('Task', {
+	state: () => {
+		return {
+			tasks: [] as Task[],
+		};
+	},
+	getters: {
+		getActiveTasks: (state) => state.tasks.filter((task) => task.isFinished!),
+	},
+	actions: {
+		async fetchTasks() {
+			const {
+				data: tasks,
+				status,
+				error,
+			} = await client.from('task').select('*').order('id');
+			if (error && status !== 200) throw new Error(error.message);
+
+			if (tasks === null) {
+				this.tasks = [];
+			}
+			this.tasks = tasks!;
+		},
+	},
+});
