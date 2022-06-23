1b8d6b575f4ecac4f141c72946aa3c835bbdc310
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-06-23 06:02:50 -0300

    set new Error component that will display when the fetch response submits in a failure

---
+++ src/components/errors/FetchError.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+	<article>
+		Ha ocurrido un error mientras se cargaban los recursos. Por favor, vuelva a
+		intentarlo más tarde.
+	</article>
+</template>
