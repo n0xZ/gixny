b53f321c8485d8d3f3a45c553c2e589aed0cdc6c
n0xZ <o.gonzalo@hotmail.com>
2022-06-26 21:22:11 -0300

    set styles

--- src/components/Layout/DashboardLayout.vue
+++ src/components/Layout/DashboardLayout.vue
@@ -4,13 +4,22 @@
 </script>
 
 <template>
-	<section
-		class="min-h-screen flex flex-col w-64  items-center h-full  rounded-md border-r-2 border-t-2 border-gray-500"
-		v-if="isLargeScreen"
-	>
-		<article class="flex flex-col space-y-3 mt-3">
-			<router-link to="/dashboard/tasks/view"> Ver tareas</router-link>
-			<router-link to="/dashboard/tasks/create">Crear tareas</router-link>
+	<div class="flex flex-row w-full" v-if="isLargeScreen">
+		<article
+			class="min-h-screen flex flex-col w-64 items-center h-full rounded-md border-r-2 border-t-2 border-zinc-900 bg-zinc-900 c-gray-300 font-bold"
+		>
+			<aside class="flex flex-col space-y-3 mt-3">
+				<router-link
+					to="/dashboard/tasks/view"
+					class="flex flex-row space-x-2 hover:c-amber duration-150 ease-in"
+					><div area-label="View task Icon"></div>
+					<span>Ver tareas</span></router-link
+				>
+				<router-link to="/dashboard/tasks/create">Crear tareas</router-link>
+			</aside>
 		</article>
-	</section>
+
+		<slot></slot>
+	</div>
+	<slot v-if="!isLargeScreen"></slot>
 </template>
